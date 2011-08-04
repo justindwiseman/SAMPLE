b4d6c5f588a0aa64df6eec0e922cb8cf7d60e54c
justindw <justindwiseman@gmail.com>
2011-08-04 09:12:41 -0500

    adding rails files to SAMPLE

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SAMPLE::Application.config.secret_token = '7d7b28cd3cbba765b40c9f52ef13ba4f4e3e4ec872dba90e29b4af5e2d72091265dfa902c7523b1ced9b2e72d0e1ffc6539179f50a681e1da2353303e58e9f41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SAMPLE::Application.config.session_store :cookie_store, :key => '_SAMPLE_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SAMPLE::Application.config.session_store :active_record_store
